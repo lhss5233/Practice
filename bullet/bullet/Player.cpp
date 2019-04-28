66762028ec00595098125c71378dc5462b25a521
Leeseonwoo11 <sw0679@naver.com>
2019-04-28 15:50:30 +0900

    bullet
    기능추가
    
    강선강화 : 총위력이 세짐 ('뚜아아아앙' 으로 표현)
    쿨러 : 과열까지의 시간을 더 벌어주고 냉각속도 빠르게함
    확장탄창 : 총알 10발 더 쏠수있음

--- bullet/bullet/Player.cpp
+++ bullet/bullet/Player.cpp
@@ -30,39 +30,147 @@ void Player::Reload()
 }
 
 void Player::Bang()
-{	
-	if (IsOverHeat())
-		return;
-	if (m_nIndex >= m_nMax)
+{
+	if (reinforce)
 	{
-		std::cout << "총알이 부족합니다. 재장전해주세요." << std::endl;;
-		return;
+		if (IsOverHeat())
+			return;
+		if (extended_magazine)
+		{
+			if (m_nIndex >= m_nMax)
+			{
+				std::cout << "총알이 부족합니다. 재장전해주세요." << std::endl;;
+				return;
+			}
+			m_nHeat += m_Clip[m_nIndex]->m_nHeat;
+
+			m_pPool->returnBullet(m_Clip[m_nIndex]);
+			m_Clip[m_nIndex++] = nullptr;
+
+			std::cout << "뚜우우우왕" << std::endl;
+		}
+		else
+		{
+			if (m_nIndex >= (m_nMax-10))
+			{
+				std::cout << "총알이 부족합니다. 재장전해주세요." << std::endl;;
+				return;
+			}
+			m_nHeat += m_Clip[m_nIndex]->m_nHeat;
+
+			m_pPool->returnBullet(m_Clip[m_nIndex]);
+			m_Clip[m_nIndex++] = nullptr;
+
+			std::cout << "뚜우우우왕" << std::endl;
+		}
 	}
-	m_nHeat += m_Clip[m_nIndex]->m_nHeat;
-	
-	m_pPool->returnBullet(m_Clip[m_nIndex]);
-	m_Clip[m_nIndex++] = nullptr;
-	
-	std::cout << "빵" << std::endl;
+	else
+	{
+		if (IsOverHeat())
+			return;
+		if (extended_magazine)
+		{
+			if (m_nIndex >= m_nMax)
+			{
+				std::cout << "총알이 부족합니다. 재장전해주세요." << std::endl;;
+				return;
+			}
+			m_nHeat += m_Clip[m_nIndex]->m_nHeat;
+
+			m_pPool->returnBullet(m_Clip[m_nIndex]);
+			m_Clip[m_nIndex++] = nullptr;
+
+			std::cout << "빵" << std::endl;
+		}
+		else
+		{
+			if (m_nIndex >= (m_nMax-10))
+			{
+				std::cout << "총알이 부족합니다. 재장전해주세요." << std::endl;;
+				return;
+			}
+			m_nHeat += m_Clip[m_nIndex]->m_nHeat;
+
+			m_pPool->returnBullet(m_Clip[m_nIndex]);
+			m_Clip[m_nIndex++] = nullptr;
+
+			std::cout << "빵" << std::endl;
+		}
+
+	}
 }
 
 bool Player::IsOverHeat()
 {
-	if (m_bHeatFlag == false && m_nHeat >= m_nMaxHeat)
-		m_bHeatFlag = true;
-	
-	if (m_bHeatFlag)
+	if (iscooler)
 	{
-		if (m_nHeat <= 0)
-			m_bHeatFlag = false;
-		std::cout << "과열상태입니다. " << std::endl;
-		m_nHeat -= 10;
+		if (m_bHeatFlag == false && m_nHeat >= m_nMaxHeat*2)
+			m_bHeatFlag = true;
+
+		if (m_bHeatFlag)
+		{
+			if (m_nHeat <= 0)
+				m_bHeatFlag = false;
+			std::cout << "과열상태입니다. " << std::endl;
+			m_nHeat -= 20;
+		}
 	}
+	else
+	{
+		if (m_bHeatFlag == false && m_nHeat >= m_nMaxHeat)
+			m_bHeatFlag = true;
+
+		if (m_bHeatFlag)
+		{
+			if (m_nHeat <= 0)
+				m_bHeatFlag = false;
+			std::cout << "과열상태입니다. " << std::endl;
+			m_nHeat -= 10;
+		}
+	}
+
 	
 		
 
 	return m_bHeatFlag;
 }
+
+void Player::incooler()
+{
+	std::cout << "쿨러장착 " << std::endl;
+	iscooler = true;
+}
+void Player::uncooler()
+{
+	std::cout << "쿨러헤제 " << std::endl;
+	iscooler = false;
+}
+void Player::inreinforce()
+{
+	std::cout << "강선강화" << std::endl;
+	reinforce = true;
+}
+void Player::unreinforce()
+{
+	std::cout << "강선강화헤제" << std::endl;
+	reinforce = false;
+}
+
+void Player::inExtended_magazine()
+{
+	std::cout << "확장탄창장착" << std::endl;
+	extended_magazine = true;
+}
+void Player::unExtended_magazine()
+{
+	std::cout << "확장탄창헤제" << std::endl;
+	extended_magazine = false;
+}
+
+
+
+
+
 
 Player::Player()
 {
--- bullet/bullet/bulletmain.cpp
+++ bullet/bullet/bulletmain.cpp
@@ -8,49 +8,56 @@ using namespace std;
 int main()
 {
 	Player _player;
+	//기본, 쿨러X, 강선강화X, 확장탄창 X
+	for (int i = 0; i < 20; i++) 
+	{
+		_player.Bang();
+	}
+	_player.Reload();
+	for (int i = 0; i < 20; i++)
+	{
+		_player.Bang();
+	}
+	
+	_player.Reload();
+	getchar();
 
-	for (int i = 0; i < 10; i++)
+
+	// 쿨러O , 강선강화O, 확장탄창O
+	_player.incooler(); 
+	_player.inreinforce();
+	_player.inExtended_magazine();
+	for (int i = 0; i < 20; i++) 
 	{
 		_player.Bang();
 	}
 	_player.Bang();
-
 	_player.Reload();
-/*
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 20; i++)
 	{
 		_player.Bang();
 	}
-	cout << endl;
-
-	_player.Reload();
-	*/
-	for (int i = 0; i < 10; i++)
-	{
-		_player.Bang();
-	}
-
-	for (int i = 0; i < 10; i++)
-	{
-		_player.Bang();
-	}
-
 	_player.Bang();
-
 	_player.Reload();
+	for (int i = 0; i < 20; i++)
+	{
+		_player.Bang();
+	}
+
+	getchar();
+
 	for (int i = 0; i < 10; i++)
 	{
 		_player.Bang();
 	}
+	_player.Reload();
+	//강화,확탄,쿨러 헤제
+	_player.uncooler();
+	_player.unreinforce();
+	_player.unExtended_magazine();
+	for (int i = 0; i < 30; i++)
+	{
+		_player.Bang();
+	}
+
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
